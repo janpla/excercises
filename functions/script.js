d0a876a23b0908a4681c4209d54811cc693345a2
janpla <komunikacia.jajopi@gmail.com>
2020-10-03 15:49:41 +0200

    Created

---
+++ functions/script.js
@@ -0,0 +1,10 @@
+/*let printArray = (array) => {
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}*/
+let array1 = [10, 20, 30, 50, 12];
+
+array1.forEach((number) => {
+    console.log(number);
+});
