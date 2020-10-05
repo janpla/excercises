ddd579505eb379304d50946f7c2e0ecbe00518d5
janpla <komunikacia.jajopi@gmail.com>
2020-10-05 17:18:33 +0200

    Two more excercises

--- buttonOne/scripts.js
+++ buttonOne/scripts.js
@@ -1,11 +1,17 @@
+control = 1;
+
 $("#bt1").click(() => {
-    console.log("Yeah, you clicked me")
+    console.log("Yeah, you clicked me");
 })
 
 $("#bt2").click(() => {
-    $("#bt1").text("Yeah, you clicked him")
+    $("#bt1").text("Yeah, you clicked him");
 })
 
 $("#bt3").click(() => {
-    $("button").css("background-color", "pink")
+    /*console.log($("#input1").val());*/
+    if(control === 1) {
+    $("button").css("background-color", $("#input1").val());
+    control = 0;
+    }
 })
